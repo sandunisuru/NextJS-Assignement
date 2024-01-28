0df9e6072961518b27ceafd45ddbab2e1e78c90f
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2024-01-29 00:58:43 +0530

    Image component configs added

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
     compiler: {
         styledComponents: true
-    }
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'randomuser.me',
+                port: '',
+                pathname: '/api/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
